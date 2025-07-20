6dfde178797b53080d07185fd2727e4de8ac2f35
Jaanu423 <puja.mourya575@gmail.com>
2025-07-20 18:43:10 +0530

    exchange tools

--- models/Cart.js
+++ models/Cart.js
@@ -1,34 +1,15 @@
 const mongoose = require('mongoose');
 
-// const cartSchema = new mongoose.Schema({
-//   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-//   tool: { type: mongoose.Schema.Types.ObjectId, ref: 'Tools', required: true },
-//   quantity: {
-//     type: Number,
-//     required: true,
-//     default: 1,
-//     min: [1, 'Quantity must be at least 1']
-//   }
-//   ,
-//   addedAt: { type: Date, default: Date.now }
-// });
-
-
-// models/Cart.js
 const cartSchema = new mongoose.Schema({
-  user: { 
-    type: mongoose.Schema.Types.ObjectId, 
-    ref: 'User',
-    required: true 
-  },
-  tool: { 
-    type: mongoose.Schema.Types.ObjectId, 
-    ref: 'Tool',
-    required: true 
-  },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  tool: { type: mongoose.Schema.Types.ObjectId, ref: 'Tools', required: true },
   quantity: {
     type: Number,
-    default: 1
+    required: true,
+    default: 1,
+    min: [1, 'Quantity must be at least 1']
   }
-}, { timestamps: true });
+  ,
+  addedAt: { type: Date, default: Date.now }
+});
 module.exports = mongoose.model('Cart', cartSchema);
